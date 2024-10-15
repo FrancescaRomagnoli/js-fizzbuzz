3c37c46097fa9b3df62d8e77e70c8780bc641b9e
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-15 15:35:22 +0200

    add numbers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("JS OK");
+
+// # numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  // # multipli di 3
+  if (i % 3 === 0) {
+    console.log(i, "questo e' un multiplo di 3");
+  }
+}

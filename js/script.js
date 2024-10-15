73cf3797fdb41cd487dc35451350ec97fcdc45c2
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-15 15:46:48 +0200

    add multiples

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 console.log("JS OK");
 
-// # numeri da 1 a 100
+// # number pool
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
-  // # multipli di 3
-  if (i % 3 === 0) {
-    console.log(i, "questo e' un multiplo di 3");
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
   }
 }
